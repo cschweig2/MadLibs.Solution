5094b805398ea00b306900a2e4d83cf9ed762a51
Chelsea Becker <cschweig2@gmail.com>
2020-12-14 15:10:45 -0800

    add file tree

---
+++ MadLibs/Controllers/HomeController.cs
@@ -0,0 +1,48 @@
+using Microsoft.AspNetCore.Mvc;
+using MadLibs.Models;
+
+namespace MadLibs.Controllers
+{
+  public class HomeController : Controller
+  { 
+    [Route("/")]
+    public ActionResult MadLibs() { return View(); }
+
+    // [Route("/form")]
+    // public ActionResult Form() { return View(); }
+
+    // [Route("/story")]
+    // public ActionResult Story(string nounOne, string personOne, string verbOne, string nounTwo, string holiday, string colorOne, string adjectiveOne, string colorTwo, string adjectiveTwo, string verbTwo, string vegetable, string verbThree, string personTwo, string verbFour, string verbFive, string colorThree) 
+    // {
+    //     StoryVariable myStoryVariable = new StoryVariable();
+    //     myStoryVariable.NounOne = nounOne;
+    //     myStoryVariable.PersonOne = personOne;
+    //     myStoryVariable.VerbOne = verbOne;
+    //     myStoryVariable.NounTwo = nounTwo;
+    //     myStoryVariable.Holiday = holiday;
+    //     myStoryVariable.ColorOne = colorOne;
+    //     myStoryVariable.AdjectiveOne = adjectiveOne;
+    //     myStoryVariable.ColorTwo = colorTwo;
+    //     myStoryVariable.AdjectiveTwo = adjectiveTwo;
+    //     myStoryVariable.VerbTwo = verbTwo;
+    //     myStoryVariable.Vegetable = vegetable;
+    //     myStoryVariable.VerbThree = verbThree;
+    //     myStoryVariable.PersonTwo = personTwo;
+    //     myStoryVariable.VerbFour = verbFour;
+    //     myStoryVariable.VerbFive = verbFive;
+    //     myStoryVariable.ColorThree = colorThree;
+    //     return View(myStoryVariable);
+    // }
+
+    // [Route("/postcard")]
+    // public ActionResult Postcard(string recipient, string sender, string location, string souvenir)
+    // {
+    //   LetterVariable myLetterVariable = new LetterVariable();
+    //   myLetterVariable.Recipient = recipient;
+    //   myLetterVariable.Sender = sender;
+    //   myLetterVariable.Location = location;
+    //   myLetterVariable.Souvenir = souvenir;
+    //   return View(myLetterVariable);
+    // }
+  }
+}
---
+++ MadLibs/Models/StoryVariable.cs
@@ -0,0 +1,38 @@
+namespace MadLibs.Models
+{
+    public class StoryVariable
+    {
+        public string NounOne { get; set; }
+
+        public string PersonOne { get; set; }
+
+        public string VerbOne { get; set; }
+
+        public string NounTwo { get; set; }
+
+        public string Holiday { get; set; }
+
+        public string ColorOne { get; set; }
+
+        public string AdjectiveOne { get; set; }
+
+        public string ColorTwo { get; set; }
+
+        public string AdjectiveTwo { get; set; }
+
+        public string VerbTwo { get; set; }
+
+        public string Vegetable { get; set; }
+
+        public string VerbThree { get; set; }
+
+        public string PersonTwo { get; set; }
+
+        public string VerbFour { get; set; }
+
+        public string VerbFive { get; set; }
+
+        public string ColorThree { get; set; }
+
+    }
+}
